8822b8f7b0ed6b0e99cc6c9d0aec4b211be2d16d
daniel <daniel.zapata.682@gmail.com>
2021-11-14 12:23:57 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+import mole from "./assets/mole.svg";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <Link to="">
+          <img src={mole} style={{ opacity: "0.1" }} />
+        </Link>
+        <Link to="">Carrito</Link>
       </header>
-    </div>
+      <main>
+        <h1>Soy App</h1>
+        <input type="text" />
+        <nav>
+          <Link to="">Guajolotas</Link>
+          <Link to="">Bebidas</Link>
+          <Link to="">tamales</Link>
+        </nav>
+        <Outlet />
+      </main>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/routes/products.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { NavLink, Outlet, useSearchParams } from "react-router-dom";
+
+const Products = () => {
+  return <div></div>;
+};
+
+export default Products;
